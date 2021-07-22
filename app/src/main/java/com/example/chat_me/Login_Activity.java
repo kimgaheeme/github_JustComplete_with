7cebef004b00edc6f54fa80a382d785e0d39253d
GaHee Kim <aqswefr@naver.com>
2021-07-22 15:42:08 +0900

    Signup Activity

--- app/src/main/java/com/example/chat_me/Login_Activity.java
+++ app/src/main/java/com/example/chat_me/Login_Activity.java
@@ -2,7 +2,9 @@ package com.example.chat_me;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
@@ -10,7 +12,7 @@ import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 public class Login_Activity extends AppCompatActivity {
 
     private Button login;
-    private Button signin;
+    private Button signup;
 
 
     @Override
@@ -19,7 +21,13 @@ public class Login_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         login = (Button)findViewById(R.id.login_button_login);
-        signin = (Button)findViewById(R.id.login_button_signin);
+        signup = (Button)findViewById(R.id.login_button_signup);
 
+        signup.setOnClickListener(new View.OnClickListener() {//signup 이벤트 생성
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login_Activity.this,Signup_Activity.class));//페이지 넘김 만들기
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/chat_me/model/UserModel.java
@@ -0,0 +1,5 @@
+package com.example.chat_me.model;
+
+public class UserModel {
+    public  String userName;
+}
